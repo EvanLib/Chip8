2f89ad3ccc36bbfcd1b559cd378cc68eaa9b3a6e
Evan Grayson <evan@evandoesstuff.com>
2020-10-27 16:21:00 -0400

    Added stack

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ struct CPU {
     registers: [u8; 16],
     posistion_in_memory: usize,
     memory: [u8; 4096],
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -23,8 +25,11 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
             match (c, x, y, d) {
-                (0, 0, 0, 0) => {return},
+                (0, 0, 0, 0) => return,
+                (0, 0, 0xE, 0xE) => self.ret(),
+                (0x2, _, _, _) => self.call(nnn),
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
@@ -44,6 +49,28 @@ impl CPU {
             self.registers[0xF] = 0;
         }
     }
+
+    fn call(&mut self, addr: u16) {
+        let sp = self.stack_pointer;
+        let stack = &mut self.stack;
+
+        if sp > stack.len() {
+            panic!("Stack overflow")
+        }
+
+        stack[sp] = self.posistion_in_memory as u16;
+        self.stack_pointer += 1;
+        self.posistion_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        if self.stack_pointer == 0 {
+            panic!("Stack underflow")
+        }
+
+        self.stack_pointer -= 1;
+        self.posistion_in_memory = self.stack[self.stack_pointer] as usize;
+    }
 }
 
 fn main() {
@@ -51,23 +78,27 @@ fn main() {
         registers: [0; 16],
         memory: [0; 4096],
         posistion_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
-    cpu.memory[0] = 0x80;
-    cpu.memory[1] = 0x14;
-    cpu.memory[2] = 0x80;
-    cpu.memory[3] = 0x24;
-    cpu.memory[4] = 0x80;
-    cpu.memory[5] = 0x34;
+    cpu.memory[0x000] = 0x21;
+    cpu.memory[0x001] = 0x00;
+    cpu.memory[0x002] = 0x21;
+    cpu.memory[0x003] = 0x00;
 
+    cpu.memory[0x100] = 0x80;
+    cpu.memory[0x101] = 0x14;
+    cpu.memory[0x102] = 0x80;
+    cpu.memory[0x103] = 0x14;
+    cpu.memory[0x104] = 0x00;
+    cpu.memory[0x105] = 0xEE;
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
 }
