1bc78400f17f18c0b6ef2ad66db34f8018fc042d
Evan Grayson <evangrayson95@gmail.com>
2020-10-30 15:48:18 -0400

    OPCODES 0-8 implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 struct CPU {
-    // 16 8bit registers 
+    // 16 8bit registers
     registers: [u8; 16],
 
     // Program Counter
@@ -31,52 +31,36 @@ impl CPU {
         let x = ((opcode & 0x0F00) >> 8) as u8;
         let y = ((opcode & 0x00F0) >> 4) as u8;
         let d = ((opcode & 0x000F) >> 0) as u8;
-
+        let nn = (opcode & 0x00FF) as u8;
         let nnn = opcode & 0x0FFF;
+
         match (c, x, y, d) {
             (0, 0, 0, 0) => return,
             (0, 0, 0xE, 0xE) => self.ret(),
             (0x1, _, _, _) => self.jump(nnn),
             (0x2, _, _, _) => self.call(nnn),
-            (0x8, _, _, 0x4) => self.add_xy(x, y),
+            (0x3, _, _, _) => self.op_3xnn(x, nn),
+            (0x4, _, _, _) => self.op_4xnn(x, nn),
+            (0x5, _, _, 0x0) => self.op_5xy0(x, y),
+            (0x6, _, _, 0x0) => self.op_6xnn(x, nn),
+            (0x7, _, _, 0x0) => self.op_7xnn(x, nn),
+            (0x8, _, _, 0x0) => self.op_8xy0(x, y),
+            (0x8, _, _, 0x1) => self.op_8xy1(x, y),
+            (0x8, _, _, 0x2) => self.op_8xy2(x, y),
+            (0x8, _, _, 0x3) => self.op_8xy3(x, y),
+            (0x8, _, _, 0x4) => self.op_8xy4(x, y),
+            (0x8, _, _, 0x5) => self.op_8xy5(x, y),
+            (0x8, _, _, 0x6) => self.op_8xy6(x, y),
+            (0x8, _, _, 0x7) => self.op_8xy7(x, y),
+            (0x8, _, _, 0xE) => self.op_8xyE(x, y),
             _ => todo!("opcode {:04x}", opcode),
         }
     }
-               
-    
 
     fn run(&mut self) {
         loop {
             let opcode = self.read_operation();
-            self.program_counter += 2;
-            let c = ((opcode & 0xF000) >> 12) as u8;
-            let x = ((opcode & 0x0F00) >> 8) as u8;
-            let y = ((opcode & 0x00F0) >> 4) as u8;
-            let d = ((opcode & 0x000F) >> 0) as u8;
-
-            let nnn = opcode & 0x0FFF;
-            match (c, x, y, d) {
-                (0, 0, 0, 0) => return,
-                (0, 0, 0xE, 0xE) => self.ret(),
-                (0x1, _, _, _) => self.jump(nnn),
-                (0x2, _, _, _) => self.call(nnn),
-                (0x8, _, _, 0x4) => self.add_xy(x, y),
-                _ => todo!("opcode {:04x}", opcode),
-            }
-        }
-    }
-
-    fn add_xy(&mut self, x: u8, y: u8) {
-        let arg1 = self.registers[x as usize];
-        let arg2 = self.registers[y as usize];
-
-        let (val, overflow_detected) = arg1.overflowing_add(arg2);
-        self.registers[x as usize] = val;
-
-        if overflow_detected {
-            self.registers[0xF] = 1;
-        } else {
-            self.registers[0xF] = 0;
+            self.run_operation(opcode);
         }
     }
 
@@ -105,6 +89,135 @@ impl CPU {
     fn jump(&mut self, addr: u16) {
         // set program_counter to addr
         self.program_counter = addr as usize;
+    }
+
+    /// Skip the following instruction if the value of register VX equals NN
+    fn op_3xnn(&mut self, x: u8, nn: u8) {
+        let value = self.registers[x as usize];
+        if value == nn {
+            self.program_counter += 2;
+        }
+    }
+
+    /// Skip the following instruction if the value of register VX is not equal to NN
+    fn op_4xnn(&mut self, x: u8, nn: u8) {
+        let value = self.registers[x as usize];
+        if value != nn {
+            self.program_counter += 2;
+        }
+    }
+
+    /// Skip the following instruction if the value of register VX is equal to the value of register VY
+    fn op_5xy0(&mut self, x: u8, y: u8) {
+        let vx = self.registers[x as usize];
+        let vy = self.registers[y as usize];
+
+        if vx == vy {
+            println!("{}", vx);
+            println!("{}", vy);
+            self.program_counter += 2;
+        }
+    }
+    /// Store number NN in register VX
+    fn op_6xnn(&mut self, x: u8, nn: u8) {
+        self.registers[x as usize] = nn;
+    }
+
+    /// Add value nn to register VX
+    fn op_7xnn(&mut self, x: u8, nn: u8) {
+        self.registers[x as usize] += nn;
+    }
+
+    /// Store the value of register VY in register VX
+    fn op_8xy0(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] = self.registers[y as usize];
+    }
+
+    /// Set VX to VX OR VY
+    fn op_8xy1(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] = self.registers[x as usize] | self.registers[y as usize];
+    }
+
+    /// Set VX to VX OR VY
+    fn op_8xy2(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] = self.registers[x as usize] & self.registers[y as usize];
+    }
+
+    /// Set VX to VX OR VY
+    fn op_8xy3(&mut self, x: u8, y: u8) {
+        self.registers[x as usize] = self.registers[x as usize] ^ self.registers[y as usize];
+    }
+
+    /// Add the value of register VY to register VX
+    /// Set VF to 01 if a carry occurs
+    /// Set VF to 00 if a carry does not occur
+    fn op_8xy4(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow_detected) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow_detected {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+
+    /// Subtract the value of register VY from register VX
+    /// Set VF to 00 if a borrow occurs
+    /// Set VF to 01 if a borrow does not occur
+    fn op_8xy5(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow_detected) = arg1.overflowing_sub(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow_detected {
+            self.registers[0xF] = 1;
+        } else {
+            self.registers[0xF] = 0;
+        }
+    }
+
+    /// Store the value of register VY shifted right one bit in register VX
+    /// Set register VF to the least significant bit prior to the shift
+    fn op_8xy6(&mut self, x: u8, y: u8) {
+        let arg2 = self.registers[y as usize];
+        let vf = arg2 & 0x01;
+
+        // shift one bit
+        self.registers[x as usize] = arg2 >> 1;
+        self.registers[0xF] = vf;
+    }
+
+    ///	Set register VX to the value of VY minus VX
+    /// Set VF to 00 if a borrow occurs
+    /// Set VF to 01 if a borrow does not occur
+    fn op_8xy7(&mut self, x: u8, y: u8) {
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow_detected) = arg2.overflowing_sub(arg1);
+        if overflow_detected {
+            self.registers[0xF] = 0;
+        } else {
+            self.registers[0xF] = 1;
+        }
+        self.registers[x as usize] = val;
+    }
+
+    /// Store the value of register VY shifted left one bit in register VX
+    /// Set register VF to the most significant bit prior to the shift
+    fn op_8xyE(&mut self, x: u8, y: u8) {
+        // shift one bit
+        let arg2: u8 = self.registers[y as usize];
+        self.registers[x as usize] = arg2 << 1;
+
+        // set register VF to most significan bit prioir to the shift.
+        self.registers[0x0F] = (self.registers[y as usize] & 0b10000000) >> 7;
     }
 }
 
--- src/processor_test.rs
+++ src/processor_test.rs
@@ -23,3 +23,231 @@ fn test_2nnn() {
     cpu.run_operation(0x2666);
     assert_eq!(cpu.program_counter, 0x0666);
 }
+
+#[test]
+fn test_3xnn() {
+    // should jump next instruction
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 1;
+    cpu.run_operation(0x3101);
+    assert_eq!(cpu.program_counter, 0x0004);
+
+    // should not jump next instruction
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 2;
+    cpu.run_operation(0x3101);
+    assert_eq!(cpu.program_counter, 0x0002);
+}
+
+#[test]
+fn test_4xnn() {
+    // should not jump next instruction
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 1;
+    cpu.run_operation(0x4101);
+    assert_eq!(cpu.program_counter, 0x0002);
+
+    // should not jump next instruction
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 2;
+    cpu.run_operation(0x4101);
+    assert_eq!(cpu.program_counter, 0x0004);
+}
+
+#[test]
+fn test_5xy0() {
+    // should jump next instruction
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 1;
+    cpu.registers[0x0002 as usize] = 1;
+    cpu.run_operation(0x5120);
+    assert_eq!(cpu.program_counter, 0x0004);
+
+    // should not jump next instruction
+    let mut cpu2 = build_cpu();
+    cpu2.registers[0x0001 as usize] = 1;
+    cpu2.registers[0x0002 as usize] = 2;
+    cpu2.run_operation(0x5120);
+    assert_eq!(cpu2.program_counter, 0x0002);
+}
+
+#[test]
+fn test_6xnn() {
+    // should set register value
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 1;
+    cpu.run_operation(0x6120);
+    assert_eq!(cpu.registers[0x0001], 0x0020);
+}
+
+#[test]
+fn test_7xnn() {
+    // should set register value
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 1;
+    cpu.run_operation(0x7120);
+    assert_eq!(cpu.registers[0x0001], 0x0021);
+}
+
+#[test]
+fn test_8xy0() {
+    // should set register value
+    let mut cpu = build_cpu();
+    // x
+    cpu.registers[0x0001 as usize] = 1;
+    // y
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8120);
+    assert_eq!(cpu.registers[0x0001], cpu.registers[0x0002]);
+}
+
+#[test]
+fn test_8xy1() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 2 | 2;
+
+    // x
+    cpu.registers[0x0001 as usize] = 2;
+    // y
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8121);
+    assert_eq!(cpu.registers[0x0001], testval);
+}
+
+#[test]
+fn test_8xy2() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 1 & 2;
+
+    // x
+    cpu.registers[0x0001 as usize] = 1;
+    // y
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8122);
+    assert_eq!(cpu.registers[0x0001], testval);
+}
+
+#[test]
+fn test_8xy3() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 2 ^ 2;
+
+    // x
+    cpu.registers[0x0001 as usize] = 2;
+    // y
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8123);
+    assert_eq!(cpu.registers[0x0001], testval);
+}
+
+#[test]
+fn test_8xy4() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 4;
+
+    // test for addition
+    cpu.registers[0x0001 as usize] = 2;
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8124);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 0);
+
+    // test for carry
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 0xff;
+    cpu.registers[0x0002 as usize] = 0x01;
+    cpu.run_operation(0x8124);
+    assert_eq!(cpu.registers[0x000f], 1);
+}
+
+#[test]
+fn test_8xy5() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 4;
+
+    // test for addition
+    cpu.registers[0x0001 as usize] = 6;
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8125);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 0);
+
+    // test for borrow
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 0x01;
+    cpu.registers[0x0002 as usize] = 0x0f;
+    cpu.run_operation(0x8125);
+    assert_eq!(cpu.registers[0x000f], 1);
+}
+
+#[test]
+fn test_8xy6() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 2 >> 1;
+
+    // test for addition
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8126);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 0);
+
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 3 >> 1;
+
+    // test for addition
+    cpu.registers[0x0002 as usize] = 3;
+    cpu.run_operation(0x8126);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 1);
+}
+
+
+#[test]
+fn test_8xy7() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 2 - 2;
+    cpu.registers[0x0001 as usize] = 2;
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8127);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 1);
+
+    // should set register value
+    let mut cpu = build_cpu();
+    cpu.registers[0x0001 as usize] = 4;
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x8127);
+    assert_eq!(cpu.registers[0x000f], 0);
+}
+
+#[test]
+fn test_8xye() {
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval = 2 << 1;
+
+    // test for addition
+    cpu.registers[0x0001 as usize] = 2;
+    cpu.registers[0x0002 as usize] = 2;
+    cpu.run_operation(0x812E);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000f], 0);
+
+    // should set register value
+    let mut cpu = build_cpu();
+    let testval: u8 = 255 << 1;
+
+    // test for addition
+    cpu.registers[0x0002 as usize] = 255;
+    cpu.run_operation(0x812E);
+    assert_eq!(cpu.registers[0x0001], testval);
+    assert_eq!(cpu.registers[0x000F], 1);
+}
